bfd8c1181fa1fa31f9926e31d634ea1a0f997def
hemangjdave714 <hdave@mobiquityinc.com>
2023-02-24 16:45:24 +0530

    Adding project lombok and added annotations to the domain entities.

--- src/main/java/guruspringframework/sdjpamultidb/domain/creditcard/CreditCard.java
+++ src/main/java/guruspringframework/sdjpamultidb/domain/creditcard/CreditCard.java
@@ -1,9 +1,23 @@
 package guruspringframework.sdjpamultidb.domain.creditcard;
 
 import guruspringframework.sdjpamultidb.domain.CreditCardConverter;
-import jakarta.persistence.*;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreditCard {
 
     @Id
--- src/main/java/guruspringframework/sdjpamultidb/domain/pan/CreditCardPAN.java
+++ src/main/java/guruspringframework/sdjpamultidb/domain/pan/CreditCardPAN.java
@@ -2,10 +2,22 @@ package guruspringframework.sdjpamultidb.domain.pan;
 
 import guruspringframework.sdjpamultidb.domain.CreditCardConverter;
 import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreditCardPAN {
 
     @Id
